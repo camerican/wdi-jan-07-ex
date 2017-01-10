2a5800d128e011098fc6340676ec043ac25a95f8
Cam Crews <camcrews@gmail.com>
2017-01-10 18:05:22 -0500

    Updated JS Logic Exmamples

--- ex-js-logic/main.js
+++ ex-js-logic/main.js
@@ -2,7 +2,7 @@
 number = 1;
 // var number = parseInt(prompt("Please Enter a Number"));
 
-// var passPhrase = "open sesame";
+var passPhrase = "open sesame";
 
 if( number < 10 ) {
   // number < 10, say something about it
@@ -12,11 +12,11 @@ if( number < 10 ) {
   console.log( number + " is greater than or equal to 10");
 }
 
-// if( passPhrase === prompt( "Enter the password" ) ) {
-//   console.log( "You're admitted into the secret WDI club");
-// } else {
-//   console.log( "Sorry about that... please try again");
-// }
+if( passPhrase === prompt( "Enter the password" ).trim() ) {
+  console.log( "You're admitted into the secret WDI club");
+} else {
+  console.log( "Sorry about that... please try again");
+}
 
 //////// 
 // Our Second Example from the Logic Slides
@@ -57,10 +57,47 @@ function prize( doorNumber ) {
 
 function pickADoor() {
   var door = parseInt(prompt("Please pick a door between 1-3"));
-  console.log( "Your prize is " + prize(door));
+  console.log( "Your prize is " + prize(door))
 }
 
 pickADoor();
+
+var myFriends = ["Jarvis","Kyle","Robert","Ber"];
+
+for( var x = 0; x < myFriends.length; x+=1 ) {
+  console.log( myFriends[x] + " is my friend" );
+}
+
+
+// Sing the 99 bottles song
+
+var bottles = 99;
+var endingLyric;
+while( bottles > 0 ) {
+  // sing our song
+  if( bottles != 1 ) { 
+    endingLyric = (bottles-1) + " bottle" + pluralize(bottles-1) + " of (root) beer";
+  } else {
+    endingLyric = " No more (root) beer :'(";
+  }
+
+  console.log( bottles + " bottle" + pluralize(bottles) + " of (root) beer on the wall, " + bottles + " bottle" + pluralize(bottles) + " of (root) beer. Take one down and pass it around..." + endingLyric );
+
+  bottles--;      // decrease bottles by 1
+  // bottles-=1;
+  // bottles = bottles - 1;
+}
+
+function pluralize(number) {
+  if( number === 1 ) {
+    return "";
+  } else {
+    return "s";
+  }
+}
+
+
+
 
 
 
