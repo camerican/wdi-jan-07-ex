802aae0519a964e0f8edb7f22e2f8f9d1e2604df
Cam Crews <camcrews@gmail.com>
2017-01-17 11:11:32 -0500

    Phase 2 of Traffic Light OO

--- ex-js-oo/main.js
+++ ex-js-oo/main.js
@@ -6,10 +6,35 @@ var trafficLight1466 = {
 }
 
 // object constuctor style
-function TrafficLight( isGreen ) {
+function TrafficLight( id, isGreen ) {
+  this.id = id;
   this.states = ["red","yellow","green"];
   this.green = isGreen || false;
 }
-var trafficLight1467 = new TrafficLight(true);
+var trafficLights = [];
+for( var i = 0, id = 1467; i < 3; i++, id++) {
+  trafficLights[i] = new TrafficLight(id,true);
+}
 
 
+
+// var trafficLight1467 = new TrafficLight(1467, true);
+// var trafficLight1468 = new TrafficLight(1468, false);
+// var trafficLight1469 = new TrafficLight(1469, true);
+
+/***
+// Roberts's sample code, setting up arrays of values...
+
+function Person( height, hair, age ) {
+  this.height = Person.prototype.heightOptions[height];
+  this.hair = Person.prototype.hairOptions[hair];
+  this.age = Person.prototype.ageOptions[age];
+}
+// We're modifying the Object prototype here to define ranges of values for each option
+Person.prototype.heightOptions = ["6ft","5ft","7ft"];
+Person.prototype.hairOptions = ["Light Brown","Black","Blonde"];
+Person.prototype.ageOptions = [25,26,27,28,29,30,31,32,33,34,35];
+// And then constructing a particular person with the index
+// values...
+var robsPerson = new Person( 1, 2, 6 );
+***/
