17ee99246576bc543fa3af449943df7713b16dd8
Cam Crews <camcrews@gmail.com>
2017-01-20 10:35:45 -0500

    jQuery example init

---
+++ ex-js-jquery/main.js
@@ -0,0 +1,19 @@
+
+// document.getElementById("test").addEventListener("click",function(){
+
+// })
+// document.addEventListener("DOMContentLoaded",function(){
+  
+// })
+
+// $(document).ready(function(){
+  $("#test").click(function(){
+    $("h1").html("Happy Friday!");
+  })
+// })
+
+
+// $("#test").on("click",function(){
+
+// })
+// $("button")
