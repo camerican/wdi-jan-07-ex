077fed0a86d891a93173f066569a3fd36e44ae97
Cam Crews <camcrews@gmail.com>
2017-01-20 11:08:23 -0500

    Call stack fix

--- ex-js-jquery/main.js
+++ ex-js-jquery/main.js
@@ -9,18 +9,26 @@
 $(document).ready(function(){
   $("#test").click(function(){
     $("h1").html("Happy Friday!");
-    boxesOut();
+    for(var i = 0; i<$(".box").length; i++){
+      boxesOut($(".box")[i]);
+    }
   });
 
   // first attempt at eternal fade
   // $(".box").fadeOut(6000,function(){$(".box").fadeIn(6000);});
 });
-
-function boxesIn() {
-  $(".box").fadeIn(6000,boxesOut);
+var boxIn = 1, boxOut = 1;
+function boxesIn(el) {
+  console.log("insideBoxes in " + boxIn++);
+  $(el).slideUp(3000,function(){
+    boxesOut(el)
+  });
 }
-function boxesOut() {
-  $(".box").fadeOut(6000,boxesIn);
+function boxesOut(el) {
+  console.log("insideBoxes Out" + boxOut++);
+  $(el).slideDown(3000,function(){
+    boxesIn(el);
+  });
 }
 
 // $("#test").on("click",function(){
