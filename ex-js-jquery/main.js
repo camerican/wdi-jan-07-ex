fdf58d6d1a8f16d3d6c1a8fa937ca529d5db4a9c
Cam Crews <camcrews@gmail.com>
2017-01-20 10:57:21 -0500

    Eternal fading of boxes

--- ex-js-jquery/main.js
+++ ex-js-jquery/main.js
@@ -3,15 +3,25 @@
 
 // })
 // document.addEventListener("DOMContentLoaded",function(){
-  
+
 // })
 
 $(document).ready(function(){
   $("#test").click(function(){
     $("h1").html("Happy Friday!");
-  })
-})
+    boxesOut();
+  });
 
+  // first attempt at eternal fade
+  // $(".box").fadeOut(6000,function(){$(".box").fadeIn(6000);});
+});
+
+function boxesIn() {
+  $(".box").fadeIn(6000,boxesOut);
+}
+function boxesOut() {
+  $(".box").fadeOut(6000,boxesIn);
+}
 
 // $("#test").on("click",function(){
 
