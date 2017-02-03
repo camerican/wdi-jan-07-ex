aa4d8ab39538ad4a28fc064508e87256b3d062f9
Cam Crews <camcrews@gmail.com>
2017-02-03 15:20:39 -0500

    Char char chaaaar?

--- ruby/ex-01/test.rb
+++ ruby/ex-01/test.rb
@@ -69,11 +69,90 @@ for i in 0...roster.length
   puts "#{i}: #{roster[i]}"
 end
 
+
+
 i = 0
 while i < 5
   puts i 
   i += 1 # i = i + 1
 end
+
+arr = [35, 45, 32,0,4,-24]
+
+# def max fried_fish
+#   fried_fish.sort[-1]
+# end
+
+# def max arr
+#   # look for the largest element in the array...
+#   # one at at time 
+#   biggest = arr[0]
+#   arr.each do |num|
+#     if num > biggest
+#       biggest = num
+#     end
+#   end
+#   biggest
+# end
+
+def max arr
+  arr.reduce do |accumulater,current|
+    current > accumulater ? current : accumulater
+  end
+end
+
+def sum arr
+  arr.reduce(0) do |accumulater,current|
+    accumulater + current
+  end
+end
+
+def sum arr
+  arr.reduce &:+
+end
+
+keys = [:red,:green,:blue]
+values = [34,155,255]
+
+color = {}
+keys.each_with_index do |key,index|
+  color[key] = values[index]
+end
+
+
+puts max arr
+
+class Team
+  @@total = 0
+  attr_accesor :city, :name
+  def initialize name, city
+    @name = name
+    @city = city
+  end
+  def to_s
+    "The " + @city + " " @name
+  end
+  def self.total
+    @@total
+  end
+end
+
+class Pokemon
+  attr_reader :type, :weaknesses
+  attr_accessor :name
+  @@types = ["water","fire","leaf","rock","ice","bug"]
+  def initialize name, type, *weaknesses
+    @name = name
+    @type = @@types[type]
+    @weaknesses = []
+    weaknesses.each do |weakness|
+      @weaknesses.push @@types[weakness]
+    end
+  end
+end
+pokemon1 = Pokemon.new "Squirtle", 0, 1, 2
+
+
 
 
 
