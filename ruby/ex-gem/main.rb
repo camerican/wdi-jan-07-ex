0abad9b7ab61a98f89179f58f536610280e21740
Cam Crews <camcrews@gmail.com>
2017-02-06 17:34:12 -0500

    Now with Rainbow

--- ruby/ex-gem/main.rb
+++ ruby/ex-gem/main.rb
@@ -1,10 +1,11 @@
 # main.rb
 require 'geolocater'
+require 'rainbow'
 
-puts "Please enter the name of a website"
+puts "Please enter the name of a " + Rainbow("website").blue
 website = gets.strip
 results = `dig +short #{website}`.split("\n")
 
 geo_results = Geolocater.geolocate_ip( results[0] )
 
-puts "#{website} is located in #{geo_results['city']}, #{geo_results['region_name']} #{geo_results['country_name']}"
+puts "#{website} is located in " + Rainbow(geo_results['city']).red + ", #{geo_results['region_name']} #{geo_results['country_name']}"
